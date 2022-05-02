f622957ac074fc1c6b4ab8d15b9c08b8af1c45d9
paula <49191579+canfanga@users.noreply.github.com>
2022-05-02 14:06:17 +0100

    Add files via upload

---
+++ helper_functions.py
@@ -0,0 +1,82 @@
+import math
+import re
+import struct
+import os
+
+#Check sum using Longitudinal redundancy check 
+def checksum_calculator(packet):
+    lrc = 0
+    for b in packet:
+        lrc = (lrc + b) & 0xFF
+    return (((lrc ^ 0xFF) + 1) & 0xFF)
+
+#Decode the message from the server
+def decode(data:bytes):
+    data1 = bytearray(data)
+    header = data1[0:8]
+    messageNumber = header[0]
+    messageType = header[1]
+    numberParts = header[2]
+    progressMessage = header[3]
+    checksum = header[4]
+    data2 = bytes(data1[8:]) 
+    return(messageNumber,messageType,numberParts,progressMessage,checksum,data2)
+
+#Create a header
+def header(message_number: int, message_type: int, number_parts: int, progress_message: int, checksum: int, packet: bytes):
+    #Creating header with this structure UDP_header =  struct.pack(format, message_number, message_type, number_parts, progress_message, checksum, three 0's)
+
+    message_number = message_number.to_bytes(1, "big")      #Contains the number of the message that is being send
+    message_type = message_type.to_bytes(1, "big")          #Contains the type of the message that is being send
+    number_parts = number_parts.to_bytes(1, "big")          #The number of packets that are necessary to be created for the message to be send on it’s entirety
+    progress_message = progress_message.to_bytes(1, "big")  #Which part of the message does this package represent
+    checksum = (0).to_bytes(1, "big")                       #Checksum using LRC
+
+    UDP_header =  struct.pack("!cccccBBB", message_number, message_type, number_parts, progress_message, checksum,0,0,0)
+
+    UDP_header_packet = bytearray(UDP_header + packet)
+
+    checksum = checksum_calculator(UDP_header_packet)
+
+    UDP_header_packet[4] = checksum
+
+    #print(UDP_header_packet)
+
+    return bytes(UDP_header_packet)
+
+def readdirectory(directory_in_str):
+    directory = os.fsencode(directory_in_str)
+    final_string = ""    
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        filename = filename.replace("_"," ")
+        filename = filename.replace(".txt","")
+        filename = filename.replace("-"," ")
+        filename= filename[0]+':'+filename[2:]
+        final_string = final_string + filename  + "\n"
+    return final_string
+
+def findrecipe(id, directory_in_str):
+    directory = os.fsencode(directory_in_str)
+    receta = ""   
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.startswith(id):
+            #print("no funciona")
+            with open(os.path.join(directory_in_str, filename), "r") as f:
+                receta = "".join(f.readlines())
+    return receta
+
+    #Check the number of packets necesary to be created to send the whole message and return number and splited msg
+def packet_split(server_buffersize, clientMessage):
+    if len(clientMessage.encode())<((server_buffersize)-8):
+        return [clientMessage.encode()], 1
+        
+    division = math.ceil(len(clientMessage.encode()) / server_buffersize)
+    separateMessage = [0]*division
+
+    a = server_buffersize-8 
+    for x in range(division):
+        separateMessage[x] = clientMessage[x*a:(x*a)+a]
+    
+    return separateMessage, len(separateMessage)
---
+++ server.py
@@ -0,0 +1,91 @@
+from helper_functions import decode, header, readdirectory, findrecipe, checksum_calculator, packet_split
+import socket
+import struct
+ 
+
+#localIP= "0.0.0.0"
+localIP= "127.0.0.1"
+localPort= 42069
+bufferSize= 100 
+directory_in_str= "recipe"
+ 
+
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+ 
+print("UDP server up and listening")
+ 
+
+# Listen for incoming datagrams
+
+while(True):
+    try:
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+
+        #CAL LLEGIR CADA TROÇ AMB LO DE ADALT ROLLO CADA ITEM OCUPA UN NUMERO GOOD
+
+        udp_header = message[:16]
+        data = message[16:]
+
+        #decode tu mama
+        decodedmessage = decode(message)
+        client_message = decodedmessage[5].decode()
+
+        if decodedmessage[1] == 1:  #user's message is hello
+            packet = struct.pack("!i", bufferSize)
+            hello_packet = header(0, 1, 1, 1, checksum_calculator(packet), packet)
+            UDPServerSocket.sendto(hello_packet, address)
+
+        if decodedmessage[1] == 2:  #user's message is id
+            recipe_id = decodedmessage[1:]
+            try:
+                recipe = findrecipe(client_message, directory_in_str)
+                separateMessage, number_parts = packet_split(bufferSize, recipe)
+                
+                if number_parts == 1:
+                    packet = separateMessage[0].encode()
+                    UDPServerSocket.sendto(header(decodedmessage[0],5,1,1,0,packet), address)
+
+                else:
+                    for x in range(number_parts):
+                        packet = separateMessage[x].encode()
+                        UDPServerSocket.sendto(header(decodedmessage[0],5,number_parts,x+1,0,packet), address)
+
+                        ackfromClient = UDPServerSocket.recvfrom(bufferSize)
+                        decodedmessage = decode(ackfromClient[0])
+                        ack_reply = decodedmessage[3]
+                        #check si son iguals la x i la part rebuda si ho son nashi sino -1
+                        #if ack_reply != x:
+                         #   UDPServerSocket.sendto(header(decodedmessage[0],5,number_parts,(x-1),0,packet), address)
+
+            except:
+                #Send not found to client
+                UDPServerSocket.sendto(header(0, 8, 1, 1, checksum_calculator(bytes()),bytes()), address)
+
+        if decodedmessage[1] == 3:  #user's message is list
+            all_recipes = readdirectory(directory_in_str)
+            print(all_recipes)
+            packet = all_recipes.encode('UTF-8')
+            UDPServerSocket.sendto(header(decodedmessage[0],4,1,1,0,packet), address)
+        
+        if decodedmessage[1] == 6: #user's message is goodbye
+            adios_packet = header(0, 6, 1, 1, checksum_calculator(bytes()), bytes())
+            UDPServerSocket.sendto(adios_packet, address)
+
+        clientMsg = "Message from Client:{}".format(data)
+        header_data = "Data from Header:{}".format(udp_header)
+        clientIP  = "Client IP Address:{}".format(address[1])
+
+        print(header_data)
+        print(clientMsg)
+        print(clientIP)
+    except Exception as error:
+        #Send error message to client
+        packet = error.encode()
+        UDPServerSocket.sendto(header(0, 7, 1, 1, checksum_calculator(packet),packet), address)
